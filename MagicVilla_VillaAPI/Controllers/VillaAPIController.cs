db53cc0b31ed6a984f20ec85a5f7e96d8d84954d
kaanozsavran <kaanozsavran@hotmail.com>
2025-01-22 20:46:44 +0300

    added some comments

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -5,16 +5,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
-    [Route("api/VillaAPI")]
-    [ApiController]
+    [Route("api/VillaAPI")] // For route
+    [ApiController] 
     public class VillaAPIController : ControllerBase
     {
-        [HttpGet]
+        [HttpGet] //For get method
         public IEnumerable<VillaDTO> GetVillas() {
             return VillaStore.villaList;
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}")] // you can use like just id
         public VillaDTO GetVilla(int id)
         {
             return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
