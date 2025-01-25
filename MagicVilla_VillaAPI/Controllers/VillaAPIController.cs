7d43e002b8e29e359594ab8b9db8a28e04859cbd
kaanozsavran <kaanozsavran@hotmail.com>
2025-01-25 16:15:50 +0300

    added status codes for VillaAPIController.cs

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -10,14 +10,23 @@ namespace MagicVilla_VillaAPI.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet] //For get method
-        public IEnumerable<VillaDTO> GetVillas() {
-            return VillaStore.villaList;
+        public ActionResult <IEnumerable<VillaDTO>> GetVillas() {
+            return Ok(VillaStore.villaList);
         }
 
         [HttpGet("{id:int}")] // you can use like just id
-        public VillaDTO GetVilla(int id)
+        public ActionResult <VillaDTO> GetVilla(int id)
         {
-            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if(id == 0)
+            {
+                return BadRequest();
+            }           
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
         }
     }
 }
